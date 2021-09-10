536972415097ceaba3c0b320a39c855d83988f66
IRWO1 <dawidekbloch@gmail.com>
2021-09-10 15:09:14 +0200

    Zadanie

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-let num1 = 2 ;
-let num2 = 5 ;
+let num1 = 2;
+let num2 = 5;
 
-    function liczba(number1, number2){
-        let = wynik = Math.max(number1,number2);
-        return wynik;
-    }
+function liczba(number1, number2) {
+    let wynik = Math.max(number1, number2);
+    return wynik;
+}
+
 console.log(liczba(num1, num2));
